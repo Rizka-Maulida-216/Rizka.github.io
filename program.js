f1046230420770156323bcefa88362f89d918524
Rizka-Maulida-216 <70867138+Rizka-Maulida-216@users.noreply.github.com>
2020-12-11 09:12:57 +0700

    Create program.js

---
+++ program.js
@@ -0,0 +1,7 @@
+var n = prompt("Masukkan nama Anda:");
+var c= confirm ("Hai "+n+"! Apakah hari ini Anda sudah sholat?");
+if (c == true) {
+    alert('Semoga tetap Istiqomah ya!');
+}else{
+    alert('Mengapa?')
+}
